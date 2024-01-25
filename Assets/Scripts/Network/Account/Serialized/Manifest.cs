5dce9adb9d90635a8abc2d18d6061648935dbc69
Greenball233 <3120393927@qq.com>
2024-01-25 10:50:52 +0800

    Update Manifest.cs

--- Assets/Scripts/Network/Account/Serialized/Manifest.cs
+++ Assets/Scripts/Network/Account/Serialized/Manifest.cs
@@ -4,14 +4,19 @@ namespace Network.Account.Serialized
 {
     public class Manifest
     {
-        public string name { get; set; }
-        public string english_name { get; set; }
-        public string space_english_name { get; set; }
-        public string domain { get; set; }
-        public string protocol { get; set; }
-        public string agreement { get; set; }
-        public string iconurl { get; set; }
-        public ApiURL apiURL { get; set; }
+        [JsonProperty("name")] public string name { get; set; }
+        [JsonProperty("english_name")] public string english_name { get; set; }
+        [JsonProperty("space_english_name")] public string space_english_name { get; set; }
+        [JsonProperty("domain")] public string domain { get; set; }
+        [JsonProperty("protocol")] public string protocol { get; set; }
+        [JsonProperty("iconurl")] public string iconurl { get; set; }
+        [JsonProperty("apiOnline")] public ApiOnline apiOnline { get; set; }
+        [JsonProperty("apiURL")] public ApiURL apiURL { get; set; }
+    }
+
+    public class ApiOnline
+    {
+        [JsonProperty("chart")] public bool chart { get; set; }
     }
 
     public class ApiURL
@@ -19,6 +24,8 @@ namespace Network.Account.Serialized
         [JsonProperty("user-login")] public string userlogin { get; set; }
 
         [JsonProperty("user-verify")] public string userverify { get; set; }
+        [JsonProperty("chart-domain")] public string chartHost { get; set; }
+        [JsonProperty("chart-port")] public int chartPort { get; set; }
     }
     
 //     {
