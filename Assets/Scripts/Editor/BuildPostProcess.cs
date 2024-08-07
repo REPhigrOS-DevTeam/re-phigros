391ca8d44967248eb0acd070d2f7ce99164273d5
Greenball233 <3120393927@qq.com>
2024-08-07 21:17:23 +0800

    笑点解析：全删了

--- Assets/Scripts/Editor/BuildPostProcess.cs
+++ Assets/Scripts/Editor/BuildPostProcess.cs
@@ -1,6 +1,6 @@
-﻿/*using System.IO;
+﻿using System.IO;
 using UnityEditor;
-using UnityEditor.Build;
+// using UnityEditor.Build;
 using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
 
@@ -12,33 +12,33 @@ namespace Editor
         public static void OnPostprocessBuild(BuildTarget target, string path)
         {
             if (target != BuildTarget.iOS) return;
-            string applicationIdentifier = PlayerSettings.GetApplicationIdentifier(NamedBuildTarget.FromBuildTargetGroup(BuildPipeline.GetBuildTargetGroup(target)));
+            // string applicationIdentifier = PlayerSettings.GetApplicationIdentifier(NamedBuildTarget.FromBuildTargetGroup(BuildPipeline.GetBuildTargetGroup(target)));
             PlistDocument plist = new PlistDocument();
             plist.ReadFromString(File.ReadAllText(Path.Combine(path, "info.plist")));
             PlistElementDict infoDict = plist.root;
-            PlistElementArray documentTypes = infoDict.CreateArray("CFBundleDocumentTypes");
-            PlistElementDict chartArchiveDocumentType = documentTypes.AddDict();
-            chartArchiveDocumentType.SetString("CFBundleTypeName", "Chart Archive");
-            chartArchiveDocumentType.SetString("CFBundleTypeRole", "Default");
-            PlistElementArray contentTypes = chartArchiveDocumentType.CreateArray("LSItemContentTypes");
-            contentTypes.AddString("com.pkware.zip-archive");
-            contentTypes.AddString($"{applicationIdentifier}.pez");
             infoDict.SetBoolean("UIFileSharingEnabled", true); // 对第三方软件（例：iTunes）开放读写文件权限
             infoDict.SetBoolean("UISupportsDocumentBrowser", true); // 对iOS系统APP“文件”开放读写文件权限
-            PlistElementArray UTIs = infoDict.CreateArray("UTExportedTypeDeclarations");
-            PlistElementDict UTI = UTIs.AddDict();
-            PlistElementArray conformsTo = UTI.CreateArray("UTTypeConformsTo");
-            conformsTo.AddString("public.data");
-            conformsTo.AddString("public.archive");
-            UTI.SetString("UTTypeDescription", "RE:PhiEdit Chart Archive");
-            PlistElementArray _ = UTI.CreateArray("UTTypeIconFiles"); // 咱们没有图标文件desu
-            UTI.SetString("UTTypeIdentifier", $"{applicationIdentifier}.pez");
-            PlistElementDict typeIdentifiers = UTI.CreateDict("UTTypeTagSpecification");
-            PlistElementArray fileExtensions = typeIdentifiers.CreateArray("public.filename-extension");
-            fileExtensions.AddString("pez");
-            PlistElementArray mimeTypes = typeIdentifiers.CreateArray("public.mime-type");
-            mimeTypes.AddString("pez");
+            // PlistElementArray documentTypes = infoDict.CreateArray("CFBundleDocumentTypes");
+            // PlistElementDict chartArchiveDocumentType = documentTypes.AddDict();
+            // chartArchiveDocumentType.SetString("CFBundleTypeName", "Chart Archive");
+            // chartArchiveDocumentType.SetString("CFBundleTypeRole", "Default");
+            // PlistElementArray contentTypes = chartArchiveDocumentType.CreateArray("LSItemContentTypes");
+            // contentTypes.AddString("com.pkware.zip-archive");
+            // contentTypes.AddString($"{applicationIdentifier}.pez");
+            // PlistElementArray UTIs = infoDict.CreateArray("UTExportedTypeDeclarations");
+            // PlistElementDict UTI = UTIs.AddDict();
+            // PlistElementArray conformsTo = UTI.CreateArray("UTTypeConformsTo");
+            // conformsTo.AddString("public.data");
+            // conformsTo.AddString("public.archive");
+            // UTI.SetString("UTTypeDescription", "RE:PhiEdit Chart Archive");
+            // PlistElementArray _ = UTI.CreateArray("UTTypeIconFiles"); // 咱们没有图标文件desu
+            // UTI.SetString("UTTypeIdentifier", $"{applicationIdentifier}.pez");
+            // PlistElementDict typeIdentifiers = UTI.CreateDict("UTTypeTagSpecification");
+            // PlistElementArray fileExtensions = typeIdentifiers.CreateArray("public.filename-extension");
+            // fileExtensions.AddString("pez");
+            // PlistElementArray mimeTypes = typeIdentifiers.CreateArray("public.mime-type");
+            // mimeTypes.AddString("pez");
             plist.WriteToFile(Path.Combine(path, "info.plist"));
         }
     }
-}*/
+}
